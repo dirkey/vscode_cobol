45d457def9bf1bd1a3ae8a25a7a06fb49511ed01
Dirk Hoffmann <dirk.hoffmann@asado.de>
2025-08-27 23:13:51 +0200

    remove duplicate method

--- src/vscobolfolders.ts
+++ src/vscobolfolders.ts
@@ -2,77 +2,41 @@ import { workspace, WorkspaceFolder } from "vscode";
 import { ICOBOLSettings } from "./iconfiguration";
 
 export class VSWorkspaceFolders {
+    /**
+     * Get workspace folders filtered by "file" scheme with applied order.
+     */
     public static get(settings: ICOBOLSettings): ReadonlyArray<WorkspaceFolder> | undefined {
-
-        const ws = workspace.workspaceFolders;
-        if (ws === undefined) {
-            return ws;
-        }
-
-        const requiredSchema = "file";
-        const folders_order = settings.workspacefolders_order;
-
-        // make a map of the folders
-        const folderMap: Map<string, WorkspaceFolder> = new Map<string, WorkspaceFolder>();
-        for (const folder of ws) {
-            if (folder.uri.scheme === requiredSchema) {             // filter out another other than file related
-                folderMap.set(folder.name, folder);
-            }
-        }
-
-        const rwFolder: WorkspaceFolder[] = [];
-        if (folders_order.length !== 0) {
-            for (const folder of folders_order) {
-                const f = folderMap.get(folder);
-                if (f !== undefined) {
-                    rwFolder.push(f);
-                    folderMap.delete(folder);
-                }
-            }
-        }
-
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        for (const [, v] of folderMap) {
-            rwFolder.push(v);
-        }
-
-        const f: ReadonlyArray<WorkspaceFolder> = rwFolder;
-        return f;
+        return this.getFiltered("file", settings);
     }
 
-    public static get_filtered(requiredSchema:string, settings: ICOBOLSettings): ReadonlyArray<WorkspaceFolder> | undefined {
+    /**
+     * Get workspace folders filtered by schema with applied order.
+     */
+    public static getFiltered(requiredSchema: string, settings: ICOBOLSettings): ReadonlyArray<WorkspaceFolder> | undefined {
         const ws = workspace.workspaceFolders;
-        if (ws === undefined) {
-            return ws;
-        }
+        if (!ws) return undefined;
 
-        const folders_order = settings.workspacefolders_order;
+        // Build a map of workspace folders, optionally filtering by schema
+        const folderMap = new Map<string, WorkspaceFolder>(
+            ws
+                .filter(folder => !requiredSchema || folder.uri.scheme === requiredSchema)
+                .map(folder => [folder.name, folder])
+        );
 
-        // make a map of the folders
-        const folderMap: Map<string, WorkspaceFolder> = new Map<string, WorkspaceFolder>();
-        for (const folder of ws) {
-            if (requiredSchema.length === 0 || folder.uri.scheme === requiredSchema) {
-                folderMap.set(folder.name, folder);
+        const orderedFolders: WorkspaceFolder[] = [];  
+        const { workspacefolders_order: foldersOrder } = settings;
+        // Add explicitly ordered folders first
+        for (const name of foldersOrder) {
+            const folder = folderMap.get(name);
+            if (folder) {
+                orderedFolders.push(folder);
+                folderMap.delete(name);
             }
         }
 
-        const rwFolder: WorkspaceFolder[] = [];
-        if (folders_order.length !== 0) {
-            for (const folder of folders_order) {
-                const f = folderMap.get(folder);
-                if (f !== undefined) {
-                    rwFolder.push(f);
-                    folderMap.delete(folder);
-                }
-            }
-        }
+        // Add remaining folders
+        orderedFolders.push(...folderMap.values());
 
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        for (const [, v] of folderMap) {
-            rwFolder.push(v);
-        }
-
-        const f: ReadonlyArray<WorkspaceFolder> = rwFolder;
-        return f;
+        return orderedFolders;
     }
 }
